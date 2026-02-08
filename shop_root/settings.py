cad74f936982fd715ee3f2c9a51794883f08d9b6
Rahu <rahu.exp@gmail.com>
2026-02-08 17:30:24 +0600

    Set Stripe settings.

--- shop_root/settings.py
+++ shop_root/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -137,3 +139,8 @@ CART_SESSION_ID = 'cart'
 
 #Emailbackend for now to the console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#Stripe settings
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_API_VERSION = '2026-01-29'

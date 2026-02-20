7d4f382fa9eb5bf68a91eb520fa6cbcb3f6bed18
Rahu <rahu.exp@gmail.com>
2026-02-20 21:15:49 +0600

    Create admin_order_pdf which create pdf.

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, redirect
+import weasyprint
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from .forms import OrderCreationForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .task import order_created
 from cart.cart import Cart
 
@@ -48,3 +54,31 @@ def order_create(request):
         template = 'orders/create.html'
         context = {'form': form, 'cart': cart}
         return render(request, template, context)
+
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+
+    return render(request, 'admin/orders/order/order_detail.html', {'order': order})
+
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+
+    # Retrive the order object and Render the template with necessary variables
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+
+    # Find the css file
+    css_file = finders.find('shop/css/pdf.css')
+
+    # Feed it to WeasyPrint
+    pdf = weasyprint.HTML(string=html).write_pdf(stylesheets=[weasyprint.CSS(filename=css_file)])
+
+    # Return it as response
+    response = HttpResponse(content=pdf, content_type='application/pdf')
+    response['Content-Disposition'] = f'inline; filename="order-{order.id}.pdf"'
+    return response

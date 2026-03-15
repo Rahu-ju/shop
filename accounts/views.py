0a07837855725a786f739e3a09b1e5c5678aefd7
Rahu <rahu.exp@gmail.com>
2026-03-15 23:31:21 +0600

    Create the redister view.

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
-from .forms import LoginForm
+from .forms import LoginForm, UserResgistrationForm
 
 
 
@@ -32,3 +32,21 @@ def user_login(request):
 def dashboard(request):
     dashboard = None
     return render(request, 'accounts/dashboard.html', {'dashboard': dashboard} )
+
+
+
+def register(request):
+    if request.method == 'POST':
+
+        # take form data and check its valid or not
+        form = UserResgistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            # before save, feed the password to the model form
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+
+            return render(request, 'accounts/registration_done.html', {'new_user': new_user} )
+    else:
+        form = UserResgistrationForm()
+        return render(request, 'accounts/registration.html', {'form': form})

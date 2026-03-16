2dce0da6f16f83b16aa788348beb0864b7510145
Rahu <rahu.exp@gmail.com>
2026-03-16 19:18:43 +0600

    Extend User model with profile modeland give its flow.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponse
 
-from .forms import LoginForm, UserResgistrationForm
+from .forms import LoginForm, UserResgistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 
@@ -42,11 +45,37 @@ def register(request):
         form = UserResgistrationForm(request.POST)
         if form.is_valid():
             new_user = form.save(commit=False)
+
             # before save, feed the password to the model form
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
+
+            # create profile objects of the new user
+            Profile.objects.create(user=new_user)
 
             return render(request, 'accounts/registration_done.html', {'new_user': new_user} )
     else:
         form = UserResgistrationForm()
         return render(request, 'accounts/registration.html', {'form': form})
+
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            # send message to the user.
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile.')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, 'accounts/edit.html', {'user_form': user_form, 'profile_form': profile_form})

1c6727bb75cb3b8ad87fe80235a59beb1d7cb094
Rahu <rahu.exp@gmail.com>
2026-03-18 15:39:30 +0600

    Writes two methods one for check passwrod matching, another email matching agains databse.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 from .models import Profile
 
@@ -12,6 +13,10 @@ class LoginForm(forms.Form):
 
 
 class UserResgistrationForm(forms.ModelForm):
+    '''
+    when form.is_valid() function is called then all the methods of this class will 
+    called and raise error accordingly if any.
+    '''
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
 
@@ -20,10 +25,24 @@ class UserResgistrationForm(forms.ModelForm):
         fields = ['username', 'first_name', 'email']
 
     def clean_password2(self):
+        '''
+        This method raise error if two password are not matched.
+        '''
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError("Your Passwords don't match")
         return cd['password2']
+    
+    def clean_email(self):
+        '''
+        This method raise error if the new user's email already in the databse.
+        '''
+        email_data = self.cleaned_data['email']
+        qs = User.objects.filter(email=email_data)
+
+        if qs.exists():
+            raise forms.ValidationError('Email already in use')
+        return email_data
 
 
 
@@ -31,6 +50,18 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        '''
+        This method raise error when an existing user change the mail in the edit profile form
+        but it already in the database except his own.
+        '''
+        email_data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=email_data)
+
+        if qs.exists():
+            raise forms.ValidationError('The mail already in use.')
+        return email_data
 
 
 

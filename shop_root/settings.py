0e143e3450cea5b68b5bde0530781cf67371c6bb
Rahu <rahu.exp@gmail.com>
2026-04-17 12:58:02 +0600

    Used python decouple.

--- shop_root/settings.py
+++ shop_root/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4un1umrtan2q!!5nu5drj@)02(su+2tiyw)n)(gu+^lnwwo0@*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 

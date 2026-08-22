231369a24d741fd8d63e6961cf55a65b3fa18552
rahu <rahu.exp@gmail.com>
2026-08-22 22:07:27 +0600

    Add callback url for production.

--- payment/bkash.py
+++ payment/bkash.py
@@ -43,8 +43,8 @@ class BkashService:
         payload = {
             "mode": "0011",
             "payerReference": str(invoice_number),
-            # "callbackURL": "https://altas.duckdns.org/payment/bkash/callback/",
-            "callbackURL": "https://sr7f2nw9-8000.asse.devtunnels.ms/payment/bkash/callback/",
+            "callbackURL": "https://altas.duckdns.org/payment/bkash/callback/",
+            # "callbackURL": "https://sr7f2nw9-8000.asse.devtunnels.ms/payment/bkash/callback/",
             "amount": str(amount),
             "currency": "BDT",
             "intent": "sale",

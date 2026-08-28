d274f6191515720cc3934adeb926d01bc237e0c6
rahu <rahu.exp@gmail.com>
2026-08-28 12:46:09 +0600

    Making it clear.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,18 @@
-from django.urls import include, path, reverse_lazy
-from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 from . import views
 
+
+
+# Define app name
 app_name = 'account'
 
 urlpatterns = [
-    path('signin/', views.signin_view, name='custom_login'),
-    # path('signin/', views.custom_login, name='custom_login'),
-    path('', include('django.contrib.auth.urls')),
-    path('', views.dashboard, name='dashboard'),
+    # path('', include('django.contrib.auth.urls')),
     path('register/', views.signup_view, name='register'),
+    path('signin/', views.signin_view, name='custom_login'),
+    path('signout/', views.signout_view, name='custom_logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('edit/', views.edit, name='edit'),
     path('verify-email/<uuid:token>/', views.verify_email, name='verify_email'),
     path('resend-email/<uuid:token>/', views.resend_email_verification, name='resend_email'),

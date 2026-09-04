753448467f58abd76c06706a17203baf5b40166e
rahu <rahu.exp@gmail.com>
2026-09-04 22:41:26 +0600

    Create right context variable.

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.utils import timezone
 from django.urls import reverse
 
-from .forms import SignUpForm, UserEditForm, ProfileEditForm
+from .forms import SignUpForm, SignInForm, UserEditForm, ProfileEditForm
 from .models import Profile
 from .task import send_verification_email
 from .models import CustomUser
@@ -44,10 +44,8 @@ def signup_view(request):
                 )
             send_verification_email.delay(verification_link, email)
 
-            # Send message
             messages.info(request, 'Verification link has been sent')
-
-            return render(request, 'templates/email_verification.html', {'new_user': new_user, 'send_link': True} )
+            return render(request, 'templates/email_verification.html', {'username': new_user.username, 'send_link': True} )
         else:
             return render(request, 'templates/signup.html', {'form': form})
 
@@ -57,21 +55,42 @@ def signup_view(request):
 
 
 def signin_view(request):
+
     if request.method == "POST":
 
-        # getting the user info from post data
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        # Feed the post data to the form
+        form = SignInForm(request.POST)
+        if form.is_valid():
 
-        # Authenticate the user
-        user = authenticate(request, username=email, password=password)
+            # Take the user object from the form, 
+            # Addded intentionally to avoid DB query again.
+            user = form.cleaned_data['user']
 
-        # log the user in
-        if user is not None:
-            login(request, user)
-            return redirect('shop:home')
-        
-        return render(request, 'templates/login.html', {'error': "invalid credentials"})
+            # log the user in if active
+            if user.is_active:
+                login(request, user)
+                return redirect('shop:home')
+
+            # depends on the token expiration, show the message to the user.
+            if user.profile.is_token_expired():
+                context = { 'verified': False, 
+                            'token': user.profile.verification_token,
+                            'username': user.username,
+                            'headline': 'Account not verified yet',
+                            'message': 'The verification time has expired. Please request a new one.'
+                            }
+                
+                return render(request, 'templates/email_verification.html', context)
+            else:
+                context = { 'verified': False,
+                            'token': user.profile.verification_token,
+                            'username': user.username,
+                            'headline': 'Email not verified yet',
+                            'message': 'You are not verified yet, Check your mail, we already sent the verification link.'
+                            }
+                return render(request, 'templates/email_verification.html', context)   
+             
+        return render(request, 'templates/login.html', {'form': form,})
     return render(request, 'templates/login.html')
 
 
@@ -117,8 +136,14 @@ def verify_email(request, token):
     # check if token expired
     if profile.is_token_expired():
 
-        messages.error(request, 'Verification link expired!!')
-        return render(request, 'templates/email_verification.html', {'token': token, 'token_expired': True,})
+        
+        context = { 'verified': False, 
+                   'token': token, 
+                   'username': profile.user.username,
+                    'headline': 'Verification link expired',
+                    'message': 'The verification link has expired. Please request a new one.'
+                   }
+        return render(request, 'templates/email_verification.html', context)
     
     # activate the account
     profile.email_verified = True
@@ -127,7 +152,7 @@ def verify_email(request, token):
     profile.save()
     
     messages.info(request, 'Account verified.')
-    return render(request, 'templates/email_verification.html', {'verified': True})
+    return render(request, 'templates/email_verification.html', {'verified': True, 'email': profile.user.email,})
 
 
 
@@ -137,13 +162,22 @@ def resend_email_verification(request, token):
     '''
     # retrive profile and user
     profile = get_object_or_404(Profile, verification_token=token)
-    user = profile.user
+    username = profile.user.username
+    email = profile.user.email
 
     # generate new token
     profile.generate_new_token()
 
-    # send the mail
-    send_verification_email(request, user)
+    # send the mail asynchronoulsy via celery
+    verification_link = request.build_absolute_uri(
+        reverse('account:verify_email', kwargs={'token': profile.verification_token})
+    )
+    send_verification_email.delay(verification_link, email)
 
     messages.info(request, 'verification link sent, once again.')
-    return render(request, 'templates/email_verification.html', {'send_link': True})    
+    context = { 'send_link': True, 
+                'username': username,
+                'headline': 'Verification link sent',
+                'message': 'We have sent the verification link to your mail, again. Please check your mail.'
+               }
+    return render(request, 'templates/email_verification.html', context)    
